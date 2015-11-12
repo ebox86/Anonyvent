5a0884c395ba9d363752c3e2313ad08d1b7212b3
Evan Kohout <ebox86@gmail.com>
2015-11-12 02:38:26 -0800

    11/12 update

--- Anonyvent/EventsHomeViewController.swift
+++ Anonyvent/EventsHomeViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Foundation
 
 class EventsHomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var events:Array<EventPost>?
-    var eventsWrapper:EventPost? // holds the last wrapper that we've loaded
+    var eventsWrapper:EventWrapper? // holds the last wrapper that we've loaded
     var isLoadingEvents = false
     
     @IBOutlet weak var EventsTableView: UITableView!
@@ -33,12 +36,38 @@ class EventsHomeViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if self.events == nil
+        {
+            return 0
+        }
+        return self.events!.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        if self.events != nil && self.events!.count >= indexPath.row
+        {
+            let species = self.events![indexPath.row]
+            cell.textLabel?.text = species.name
+            cell.detailTextLabel?.text = events?.description
+            
+            // See if we need to load more species
+            let rowsToLoadFromBottom = 5;
+            let rowsLoaded = self.events!.count
+            if (!self.isLoadingEvents && (indexPath.row >= (rowsLoaded - rowsToLoadFromBottom)))
+            {
+                let totalRows = self.eventsWrapper!.count!
+                let remainingSpeciesToLoad = totalRows - rowsLoaded;
+                if (remainingSpeciesToLoad > 0)
+                {
+                    self.loadMoreEvents()
+                }
+            }
+        }
+        
         return cell
     }
     
@@ -55,5 +84,57 @@ class EventsHomeViewController: UIViewController, UITableViewDataSource, UITable
         // Pass the selected object to the new view controller.
     }
     */
-
+    func loadFirstEvents()
+    {
+        isLoadingEvents = true
+        EventPost.getEvents { wrapper, error in
+            if let error = error
+            {
+                // TODO: improved error handling
+                self.isLoadingEvents = false
+                let alert = UIAlertController(title: "Error", message: "Could not load first event \(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            self.addEventsFromWrapper(wrapper)
+            self.isLoadingEvents = false
+            self.tableview?.reloadData()
+        }
+    }
+    
+    func loadMoreEvents()
+    {
+        self.isLoadingEvents = true
+        if self.events != nil && self.eventsWrapper != nil && self.events!.count < self.eventsWrapper!.count
+        {
+            // there are more species out there!
+            EventPost.getMoreEvents(self.eventsWrapper, completionHandler: { wrapper, error in
+                if let error = error
+                {
+                    // TODO: improved error handling
+                    self.isLoadingEvents = false
+                    let alert = UIAlertController(title: "Error", message: "Could not load more events \(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                print("got more!")
+                self.addEventsFromWrapper(wrapper)
+                self.isLoadingEvents = false
+                self.tableview?.reloadData()
+            })
+        }
+    }
+    
+    func addEventsFromWrapper(wrapper: EventsWrapper?)
+    {
+        self.eventsWrapper = wrapper
+        if self.events == nil
+        {
+            self.events = self.eventsWrapper?.event
+        }
+        else if self.eventsWrapper != nil && self.eventsWrapper!.event != nil
+        {
+            self.events = self.events! + self.eventsWrapper!.event!
+        }
+    }
 }
